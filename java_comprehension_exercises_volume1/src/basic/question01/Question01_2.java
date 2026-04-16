d44065fb28a1ba90f71fc798930f0af48988c3cb
kimurada <kimurada@C010422705.zipangu.cac.co.jp>
2026-04-16 14:47:29 +0900

    修正

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_2.java
@@ -9,15 +9,15 @@ public class Question01_2 {
 	public static void main(String[] args) {
 
 		// TODO:ここに実装
-		int appleprice = 100;
+		int applePrice = 100;
 		String stationery = "鉛筆";
 
-		int bananaPrice = appleprice;
-		String writingUtensilString = stationery;
+		int bananaPrice = applePrice;
+		String writingUtensil = stationery;
 
-		System.out.println("リンゴの値段は" + appleprice + "円です。");
+		System.out.println("リンゴの値段は" + applePrice + "円です。");
 		System.out.println("バナナの値段は" + bananaPrice + "円です。");
-		System.out.println("この筆記用具は" + writingUtensilString + "です。");
+		System.out.println("この筆記用具は" + writingUtensil + "です。");
 
 	}
 
